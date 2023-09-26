0c764c9a1580542ab9235e2bf9b0386a4f01068e
Patil Narendra Asaram <101571740+nareshpatil031886@users.noreply.github.com>
2023-09-26 19:27:26 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.jsx
@@ -0,0 +1,25 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNTmHR9E_EmMZVMK0oHTbjsviU4LF4CtY",
+  authDomain: "crypto-f0135.firebaseapp.com",
+  projectId: "crypto-f0135",
+  storageBucket: "crypto-f0135.appspot.com",
+  messagingSenderId: "886663268780",
+  appId: "1:886663268780:web:69c64b57e83b9a65bcd209",
+  measurementId: "G-531B2M2P1Z"
+};
+
+export default firebaseConfig;
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
